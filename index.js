8f1ae4515e9b5d3372119e86e7de04ddedfab274
[Mapache Code] <yuu_code@outlook.com>
2020-10-26 22:19:06 -0300

    Terminando el portafolio

---
+++ index.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+  const width = window.innerWidth;
+  if (width <= "426") {
+    const projects = document.querySelectorAll(".noShow");
+    projects.forEach((p) => {
+      p.classList.remove("noShow");
+    });
+  }
+};
+
+function previous() {
+  const projects = document.querySelectorAll(".Project");
+  let position;
+  for (let i = 0; i < projects.length; i++) {
+    if (projects[i].classList.contains("active")) {
+      position = i;
+    }
+  }
+
+  projects[position].classList.remove("active");
+  projects[position].classList.add("noShow");
+
+  if (position == 0) {
+    projects[projects.length - 1].classList.add("active");
+    projects[projects.length - 1].classList.remove("noShow");
+  } else {
+    projects[position - 1].classList.add("active");
+    projects[position - 1].classList.remove("noShow");
+  }
+}
+
+function next() {
+  const projects = document.querySelectorAll(".Project");
+  let position;
+  for (let i = 0; i < projects.length; i++) {
+    if (projects[i].classList.contains("active")) {
+      position = i;
+    }
+  }
+
+  projects[position].classList.remove("active");
+  projects[position].classList.add("noShow");
+
+  if (position >= projects.length - 1) {
+    projects[0].classList.add("active");
+    projects[0].classList.remove("noShow");
+  } else {
+    projects[position + 1].classList.add("active");
+    projects[position + 1].classList.remove("noShow");
+  }
+}
